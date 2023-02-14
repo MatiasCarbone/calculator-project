1bc6a3e3756e38c94b6eb1c28d5e5a3dab21d43b
Matías Carbone <matiascarbone2@gmail.com>
2023-02-13 22:52:49 -0300

    Add basic operator functions

---
+++ operators.js
@@ -0,0 +1,19 @@
+//This script file will containt the functions that do the mathematical operations
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  if (b == 0) {
+    return "error";
+  } else return a / b;
+};
