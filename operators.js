6ac7aadf33c1876073d027e3044b6ebe73d9705f
Matías Carbone <matiascarbone2@gmail.com>
2023-02-13 22:59:59 -0300

    Add operate function to select and perform operations

--- operators.js
+++ operators.js
@@ -1,5 +1,23 @@
 //This script file will containt the functions that do the mathematical operations
 
+const operate = function (a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+
+    case "-":
+      return subtract(a, b);
+
+    case "*":
+      return multiply(a, b);
+
+    case "/":
+      return divide(a, b);
+  }
+};
+
+/********************************************************************************/
+
 const add = function (a, b) {
   return a + b;
 };
