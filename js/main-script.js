673ed7eb8a70813eeb2b6e4bebe91ffb8192be5c
Matías Carbone <matiascarbone2@gmail.com>
2023-02-15 17:57:41 -0300

    Add updateDisplay function

--- js/main-script.js
+++ js/main-script.js
@@ -1,5 +1,14 @@
 import { operate } from "./operators.js";
 
+let displayValue = "";
+
 let operandOne = null;
 let operandTwo = null;
 let operator = null;
+
+function updateDisplay(digit) {
+  displayValue = displayValue + String(digit);
+}
+
+updateDisplay(7);
+console.log(displayValue);
